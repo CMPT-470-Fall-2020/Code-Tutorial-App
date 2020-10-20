c0a598d6228796339b7544b4932a8be53129f9a5
Emily <eca83@sfu.ca>
2020-10-19 22:21:23 -0700

    Create markdown live preview using markedjs

--- client/src/components/MarkdownEditor.js
+++ client/src/components/MarkdownEditor.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
-import {UnControlled as CodeMirror} from 'react-codemirror2';
+import {Controlled as CodeMirror} from 'react-codemirror2';
+import Button from 'react-bootstrap/Button';
 import 'codemirror/mode/markdown/markdown.js';
 import 'codemirror/lib/codemirror.css';
 import './scss/MarkdownEditor.scss';
 
+let marked = require('marked')
+
 export default class MarkupEditor extends Component {
+    
+    state = {
+        value:''
+    }
+
     render() {
         return (
             <React.Fragment>
-                <h1 style={headerStyle}>Markdown Editor</h1>
-<               CodeMirror
-                    value=''
-                    options={{
-                        mode: 'markdown',
-                        lineNumbers: true
+                <section>
+                    <h1 style={headerStyle}>Markdown Editor</h1>
+                    <Button variant="primary" style={saveStyle}>Save</Button>
+
+                </section>
+                <CodeMirror
+                    value={this.state.value}
+                    options={options}
+                    onBeforeChange={(editor, data, value) => {
+                      this.setState({value});
                     }}
                     onChange={(editor, data, value) => {
+                      this.setState({value});
                     }}
                 />
+                <div>
+                    <h1 style={headerStyle}>Markdown Preview</h1>
+                    <div style={previewStyle} dangerouslySetInnerHTML = {{__html: marked(this.state.value)}}>                               
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
@@ -25,6 +43,28 @@ export default class MarkupEditor extends Component {
 
 const headerStyle = {
     fontSize: '20px',
-    margin:'2% 1% 1% 1%',
-    fontFamily: 'Arial, Helvetica, sans-serif'
+    margin:'2% 1% 0% 1%',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    display: 'inline-block'
+}
+
+const saveStyle = {
+    padding: '3px',
+    float: 'right',
+    margin:'2% 1% 0% 1%',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    backgroundColor: '#343a40'
+}
+
+const options = {
+    mode: 'markdown',
+    lineNumbers: true
+}
+
+const previewStyle = {
+    resize: 'none',
+    margin: '1%',
+    padding: '1%',
+    border: '1px solid black',
+    minHeight: '100px'
 }
--- client/src/components/pages/Tutorial.js
+++ client/src/components/pages/Tutorial.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import MarkdownEditor from '../MarkdownEditor'
+
 export default class Tutorial extends Component {
     render() {
         return (
