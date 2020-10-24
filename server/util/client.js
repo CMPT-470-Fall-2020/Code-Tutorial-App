10605022abc7f4e5fceea5f1f5ab7594fa68d8f6
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-24 09:55:59 -0700

    Extract msg queue into new class. Refactor interpreter class.
    - Extract queue used to store messages into a separate class.
    - Refactor interpreter class constructor to use new queue class.
    - Refactor interpreter class to accept an event emitter instance and the interpreter name.
    - Refactor interpreter class constructor to store flags regarding the current state.

--- server/util/client.js
+++ server/util/client.js
@@ -1,18 +1,53 @@
 var net = require("net");
 
-class Interpreter {
-  constructor(portNum) {
+class Queue {
+  constructor() {
+    // Queue containing all messages
     this.msgQueue = [];
     this.queueLen = 0;
+  }
+
+  isEmpty() {
+    return this.queueLen == 0;
+  }
+
+  addMessage(msg) {
+    this.msgQueue.push(data);
+    this.queueLen += 1;
+  }
+
+  getMessage() {
+    if (this.queueLen > 0) {
+      let msg = this.msgQueue.shift();
+      this.queueLen -= 1;
+      return msg;
+    }
+    return undefined;
+  }
+}
+
+
+
+class Interpreter {
+  constructor(portNum, interpName, eventEmitter) {
+    this.name = interpName;
+    this.msgQueue = new Queue();
+    // Event emitter used to notify server when a request is completed and result
+    // can be sent back to caller
+    this.emitter = eventEmitter;
+    // Indicate that the socket is still open
     this.sockStatus = false;
+    // Flag indicating whether we are waiting for a response from the server
+    this.isWaiting = false;
+    // The socket used to make a connection
     this.socket = new net.Socket();
 
     this.socket.connect(portNum, "127.0.0.1", () => {
       console.log("client on port: ", portNum, " connected!");
       this.sockStatus = true;
-      if (this.queueLen > 0) {
-        let val = this.msgQueue.shift();
-        this.queueLen -= 1;
+      if (!this.msgQueue.isEmpty()) {
+        this.isWaiting = true;
+        msg = this.msgQueue.getMessage();
         this.socket.write(val);
       }
     });
