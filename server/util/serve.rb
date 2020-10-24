1aec6197f684902f49264671cb7827b15f770d5b
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-24 15:15:29 -0700

    Create basic client in ruby.

---
+++ server/util/serve.rb
@@ -0,0 +1,27 @@
+require 'socket'
+require 'json'
+require 'stringio'
+
+server = TCPServer.open(5555)  
+
+loop {
+    client = server.accept       
+    loop {    
+        $stdout = StringIO.new
+        $stderr = StringIO.new
+        buffer = client.gets()
+        if buffer.nil?
+            break
+        end
+        decoded_json = JSON.parse(buffer)
+
+        begin
+        TOPLEVEL_BINDING.eval(decoded_json['code'])
+        rescue Exception => e
+          $stderr.puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+          $stderr.puts "Error during processing: #{$!}"
+        end
+        out = {"stderr" => $stderr.string, "stdout" => $stdout.string}
+        client.puts JSON(out)
+    }
+}
