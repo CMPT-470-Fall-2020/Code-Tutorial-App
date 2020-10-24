e93ae30aadf88a17c712699a5cc3f9531d599131
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-23 20:36:53 -0700

    Make python client use only one connection.
    - Previously, the python client tried to create a new connection on
      every iteration of the loop. Now, the original connection is
      maintained.

--- server/util/serve.py
+++ server/util/serve.py
@@ -9,7 +9,7 @@ SOCKET = socket.socket(
 
 # get local machine name
 HOSTNAME = '127.0.0.1'
-PORTNUM = 8003
+PORTNUM = 8007
 # bind to the port
 SOCKET.bind((HOSTNAME, PORTNUM))
 
@@ -19,13 +19,14 @@ SOCKET.listen(5)
 
 orig_out = sys.stdout
 orig_err = sys.stderr
+clientsocket, addr = SOCKET.accept()
+
 while True:
     f1 = open("stdout", "w")
     f2 = open("stderr", "w")
     sys.stdout = f1
     sys.stderr = f2
     # establish a connection
-    clientsocket, addr = SOCKET.accept()
     command = clientsocket.recv(8096).decode('utf-8')
     command = json.loads(command)
 
@@ -35,12 +36,18 @@ while True:
         sys.stderr = orig_err
         f1.close()
         f2.close()
-        print(json.dumps({"stdout": open("stdout", 'r').read(), "stderr": open('stderr', 'r').read()}))
+
+        out = open("stdout", 'r')
+        err = open('stderr', 'r')
+        print(json.dumps({"stdout": out.read(), "stderr": err.read() }))
+        out.close()
+        err.close()
+
         clientsocket.send(b'RESPONSE!')
 
     elif command['type'] == "KILL":
+        clientsocket.close()
         sys.exit()
 
-    #clientsocket.close()
 
 
