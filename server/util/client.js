0090cc73fa567e283a4359c603d0af5558e167dd
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-23 18:31:37 -0700

    Implement barebone socket client. The current version of the socket client can send only one command.

---
+++ server/util/client.js
@@ -0,0 +1,17 @@
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(8003, '127.0.0.1', function() {
+	console.log('Connected');
+	let code = JSON.stringify({type: "RUN", code:'print(a + 4)'});
+	client.write(code);
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
