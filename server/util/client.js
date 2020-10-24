6c4719552d3eb048742f565675317087a22eec01
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-23 22:55:43 -0700

    Remove debug print statements.

--- server/util/client.js
+++ server/util/client.js
@@ -1,17 +1,48 @@
-var net = require('net');
+var net = require("net");
 
-var client = new net.Socket();
-client.connect(8003, '127.0.0.1', function() {
-	console.log('Connected');
-	let code = JSON.stringify({type: "RUN", code:'print(a + 4)'});
-	client.write(code);
-});
+class Interpreter {
+  constructor(portNum) {
+    this.msgQueue = [];
+    this.queueLen = 0;
+    this.sockStatus = false;
+    this.socket = new net.Socket();
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
+    this.socket.connect(portNum, "127.0.0.1", () => {
+      console.log("client on port: ", portNum, " connected!");
+      this.sockStatus = true;
+      if (this.queueLen > 0) {
+        let val = this.msgQueue.shift();
+        this.queueLen -= 1;
+        this.socket.write(val);
+      }
+    });
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+    this.socket.on("data", (data) => {
+      this.recv_data(data);
+    });
+
+    this.socket.on("close", function () {
+      console.log("Connection closed");
+    });
+  }
+
+  recv_data(data) {
+    if (this.queueLen > 0) {
+      let val = this.msgQueue.shift();
+      this.queueLen -= 1;
+      this.socket.write(val);
+    }
+  }
+  isLive() {
+    return this.sockStatus;
+  }
+
+  kill() {
+    this.socket.destroy();
+  }
+
+  send(data) {
+    this.msgQueue.push(data);
+    this.queueLen += 1;
+  }
+}
