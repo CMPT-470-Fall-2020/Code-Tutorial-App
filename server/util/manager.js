2a5bc0b6adf5d4dea55f5dd00e02f6870ec21299
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-25 11:29:28 -0700

    Format interpreter manager file.

--- server/util/manager.js
+++ server/util/manager.js
@@ -1,4 +1,5 @@
-const cp = require('child_process');
+const cp = require("child_process");
+const { Interpreter } = require("./client.js");
 
 // Constants used to indicate the return status of function calls
 const USERDNE = "USERDNE";
@@ -7,77 +8,77 @@ const SUCCESS = "SUCCESS";
 const INTERPDNE = "INTERPDNE";
 const INTERPEXISTS = "INTERPEXISTS";
 
-class InterpreterManager{
-	constructor(){
-		this.instances = {};
-		this.ports = []
+class InterpreterManager {
+  constructor() {
+    this.instances = {};
+    this.ports = [];
 
-		// There are 65,536 TCP ports in total
-		// Ports from 0 - 1023 are well known so we do not use those.
-		for (i = 1024; i < 65000, i++){
-			this.ports.push(i);
-		}
-	}
-	
-	userExists(userName){
-		if (username in this.instances){
-			return true;
-		}
-		return false;
-	}
+    // There are 65,536 TCP ports in total
+    // Ports from 0 - 1023 are well known so we do not use those.
+    for (i = 1024; i < 65000; i++) {
+      this.ports.push(i);
+    }
+  }
 
-	interpInstanceExists(userName, instanceName){
-		if (this.userExists(userName) && instanceName in this.instances[username]){
-				return true;
-		}
-		return false;
-	}
-	
-	getInterpInstance(userName, instanceName){
-		if (this.userExists(userName)){
-			if (this.interpInstanceExists(userName, instanceName)){
-				return this.instances[username][instanceName];
-			}else{
-				return INTERPDNE;
-			}
-		}
-		return USERDNE;
-	}
+  userExists(userName) {
+    if (username in this.instances) {
+      return true;
+    }
+    return false;
+  }
 
-	createUser(userName){
-		if (userName in this.instances){
-			return USEREXISTS;
-		}
+  interpInstanceExists(userName, instanceName) {
+    if (this.userExists(userName) && instanceName in this.instances[username]) {
+      return true;
+    }
+    return false;
+  }
 
-		this.instances[userName] = {};
-		return SUCCESS;
-	}
-	
-	createInterp(userName, interpName, interpType){
-		if (!this.userExists()){
-			this.createUser(userName);
-		}
+  getInterpInstance(userName, instanceName) {
+    if (this.userExists(userName)) {
+      if (this.interpInstanceExists(userName, instanceName)) {
+        return this.instances[username][instanceName];
+      } else {
+        return INTERPDNE;
+      }
+    }
+    return USERDNE;
+  }
 
-		if (this.interpInstanceExists(userName, interpName)){
-			return INTERPEXISTS;
-		}
+  createUser(userName) {
+    if (userName in this.instances) {
+      return USEREXISTS;
+    }
 
-		// Retrieve the port number for the server
-		let portNum = this.ports.pop();
-		let child; 
+    this.instances[userName] = {};
+    return SUCCESS;
+  }
 
-		switch(interpType){
-				case "PYTHON":
-					child = spawn("python3" ['serve.py', portNum])
-				case "RUBY":
-					child = spawn("ruby" ['serve.rb', portNum])
-				default:
-						console.log("Language does not exist")
-		}
+  createInterp(userName, interpName, interpType) {
+    if (!this.userExists()) {
+      this.createUser(userName);
+    }
 
-	  	child.on("exit", (code, signal) => {
-	  		// Return the port number on exit
-			this.ports.push(portNum);
-	  	});
-	}
+    if (this.interpInstanceExists(userName, interpName)) {
+      return INTERPEXISTS;
+    }
+
+    // Retrieve the port number for the server
+    let portNum = this.ports.pop();
+    let child;
+
+    switch (interpType) {
+      case "PYTHON":
+        child = spawn("python3"[("serve.py", portNum)]);
+      case "RUBY":
+        child = spawn("ruby"[("serve.rb", portNum)]);
+      default:
+        console.log("Language does not exist");
+    }
+
+    child.on("exit", (code, signal) => {
+      // Return the port number on exit
+      this.ports.push(portNum);
+    });
+  }
 }
