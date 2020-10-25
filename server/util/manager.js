6dd16d599e97752ae393bf8ae1a6e9ad0513f52d
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-25 11:14:22 -0700

    Implement main part of manager function for new intepreters.
    - Implement the main logic of the interpreter manager function to create new interpreters.

--- server/util/manager.js
+++ server/util/manager.js
@@ -1,11 +1,22 @@
+const cp = require('child_process');
+
+// Constants used to indicate the return status of function calls
 const USERDNE = "USERDNE";
 const USEREXISTS = "USEREXISTS";
 const SUCCESS = "SUCCESS";
 const INTERPDNE = "INTERPDNE";
+const INTERPEXISTS = "INTERPEXISTS";
 
 class InterpreterManager{
 	constructor(){
 		this.instances = {};
+		this.ports = []
+
+		// There are 65,536 TCP ports in total
+		// Ports from 0 - 1023 are well known so we do not use those.
+		for (i = 1024; i < 65000, i++){
+			this.ports.push(i);
+		}
 	}
 	
 	userExists(userName){
@@ -42,7 +53,30 @@ class InterpreterManager{
 		return SUCCESS;
 	}
 	
-	createInterp(userName, interpName){
-		// Create an interpreter for a user
+	createInterp(userName, interpName, interpType){
+		if (!this.userExists()){
+			this.createUser(userName);
+		}
+
+		if (this.interpInstanceExists(userName, interpName)){
+			return INTERPEXISTS;
+		}
+
+		// Retrieve the port number for the server
+		let portNum = this.ports.pop();
+		let child; 
+
+		switch(interpType){
+				case "PYTHON":
+					child = spawn("python3" ['serve.py', portNum])
+				case "RUBY":
+					child = spawn("ruby" ['serve.rb', portNum])
+				default:
+						console.log("Language does not exist")
+		}
+	  	child.on("exit", (code, signal) => {
+	  		// Return the port number on exit
+			this.ports.push(portNum);
+	  	});
 	}
 }
