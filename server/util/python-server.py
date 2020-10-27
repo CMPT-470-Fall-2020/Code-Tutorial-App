8a90685b87265f4d55bd8307133fe978cb380fa8
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-27 00:04:04 -0700

    Refactor python server.
    - Refactor python server to catch exceptions.
    - Create new "globals" array to store values generated by a script.

--- server/util/python-server.py
+++ server/util/python-server.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+# a global namespace for the exec function. It is a copy
+# of the default namespace of every interpreter.
+# It is created here first so the globals are not tainted
+# by the imports which follow this.
+import builtins
+execGlobals = {'__annotations__': {},
+ '__builtins__': builtins,
+ '__cached__': None,
+ '__doc__': None,
+ '__file__': 'subinstance.py',
+ '__loader__': globals()['__loader__'],
+ '__name__': '__main__',
+ '__package__': None,
+ '__spec__': None}
+
 import socket
 import json
 import sys
+import io
+import contextlib
 
 # create a socket object
 SOCKET = socket.socket(
@@ -22,29 +39,23 @@ orig_err = sys.stderr
 clientsocket, addr = SOCKET.accept()
 
 while True:
-    f1 = open("stdout", "w")
-    f2 = open("stderr", "w")
-    sys.stdout = f1
-    sys.stderr = f2
     # establish a connection
     command = clientsocket.recv(8096).decode('utf-8')
     command = json.loads(command)
 
     if command['type'] == "RUN":
-        exec(command['code'])
-        sys.stdout = orig_out
-        sys.stderr = orig_err
-        f1.close()
-        f2.close()
+        output = None
+        # Redirect STDOUT and STDERR into buffers
+        with io.StringIO() as out, io.StringIO() as err, contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
+            try:
+                exec(command['code'], execGlobals)
+            except:
+                import traceback
+                err.write(traceback.format_exc())
 
-        out = open("stdout", 'r')
-        err = open('stderr', 'r')
-        output = json.dumps({"stdout": out.read(), "stderr": err.read()})
-        out.close()
-        err.close()
-
+            output = json.dumps({"type": "SUCCESS", "stdout": out.getvalue(), "stderr": err.getvalue()})
+        
         clientsocket.send(bytes(output, 'utf-8'))
-
     elif command['type'] == "KILL":
         clientsocket.close()
         sys.exit()
