77b9d4bba8dabf38c56852db1a896de226b12077
knishibe <knishibeckingham@gmail.com>
2020-10-27 15:22:36 -0700

    sucessfully connected mongodb database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-CLUSTER_URI = "mongodb+srv://470_Project:<470_Project>@470project.c1pdi.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const path = require('path')
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 var app = express();
 const port = process.env.PORT || 4000;
+
+// Database code
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 /* THIS IS AN EXAMPLE OF HOW OUR FILES WILL BE SERVED WHEN WE UPLOAD TO GCP
 app.use(express.static(path.join(__dirname, "..","client", "build")));
