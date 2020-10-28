0fb702a5b7942ae977f7f034acd276ce3c06ceba
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-27 17:00:23 -0700

    Update python server to log actions.

--- server/util/python-server.py
+++ server/util/python-server.py
@@ -19,6 +19,9 @@ import json
 import sys
 import io
 import contextlib
+import time
+import logging
+logging.basicConfig(filename='python-server.log', level=logging.DEBUG)
 
 # create a socket object
 SOCKET = socket.socket(
@@ -32,30 +35,36 @@ SOCKET.bind((HOSTNAME, PORTNUM))
 
 # queue up to 5 requests
 SOCKET.listen(5)
-
+logging.debug('Server started listening')
 
 orig_out = sys.stdout
 orig_err = sys.stderr
 clientsocket, addr = SOCKET.accept()
 
 while True:
+    time.sleep(0.01)    
     # establish a connection
+    logging.debug('Listening for another command after sleep')
     command = clientsocket.recv(8096).decode('utf-8')
-    command = json.loads(command)
+    if command != "":
+        logging.debug('Received a request from server '+ command)
+        command = json.loads(command)
 
-    if command['type'] == "RUN":
-        output = None
-        # Redirect STDOUT and STDERR into buffers
-        with io.StringIO() as out, io.StringIO() as err, contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
-            try:
-                exec(command['code'], execGlobals)
-            except:
-                import traceback
-                err.write(traceback.format_exc())
+        if command['type'] == "RUN":
+            output = None
+            # Redirect STDOUT and STDERR into buffers
+            with io.StringIO() as out, io.StringIO() as err, contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
+                try:
+                    logging.debug('About to execute:' + command['code'])
+                    exec(command['code'], execGlobals)
+                except:
+                    import traceback
+                    err.write(traceback.format_exc())
 
-            output = json.dumps({"type": "SUCCESS", "stdout": out.getvalue(), "stderr": err.getvalue()})
-        
-        clientsocket.send(bytes(output, 'utf-8'))
-    elif command['type'] == "KILL":
-        clientsocket.close()
-        sys.exit()
+                logging.debug('Sending out response from the server' + str({"type": "SUCCESS", "stdout": out.getvalue(), "stderr": err.getvalue()}))
+                output = json.dumps({"type": "SUCCESS", "stdout": out.getvalue(), "stderr": err.getvalue()})
+            
+            clientsocket.send(bytes(output, 'utf-8'))
+        elif command['type'] == "KILL":
+            clientsocket.close()
+            sys.exit()
