b2fd3975eff318f0e3470c4635b42853ce224920
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-28 13:30:36 -0700

    Create Express function stubs for forum API.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const express = require('express');
+const path = require("path");
+const express = require("express");
 var app = express();
 const port = process.env.PORT || 4000;
 
@@ -10,26 +10,50 @@ const port = process.env.PORT || 4000;
 //   res.sendFile(path.join(__dirname, "..","client", "build", "index.html"))
 // })
 
+// Retrieve a list of all the threads for a class forum.
+app.get("/forum/:subforumId", (req, res) => {});
 
+// Retrieve all the posts for a particular thread in a particular class forum
+app.get("/forum/:subforumId/:threadId", (req, res) => {});
+// Add a new post for a particular subforum
+app.post("/forum/:subforumId/:threadId", (req, res) => {});
+// Delete a particular thread in a class forum
+app.delete("/forum/:subforumId/:threadId", (req, res) => {});
+
+// Update a particular post in a class forum thread
+app.post("/forum/:subforumId/:threadId/:postId", (req, res) => {});
+// Delete a particular post in a class forum thread
+app.delete("/forum/:subforumId/:threadId/:postId", (req, res) => {});
 
 /* Temp routing code */
 // const dashboardRouter = require('./routes/dashboard');
-app.get('/dashboard', (req, res) => {
-  res.json([{id: 1, name: 'CMPT 470'}, {id: 2, name: 'CMPT 471'}])
-})
+app.get("/dashboard", (req, res) => {
+  res.json([
+    { id: 1, name: "CMPT 470" },
+    { id: 2, name: "CMPT 471" },
+  ]);
+});
 
-app.get('/', (req, res) => {
-  res.json({message: 'Hello World from the backend server on the \"/\" route!'})
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: 'Hello World from the backend server on the "/" route!',
+  });
+});
 
-app.get('/coursesPage', (req, res) => {
-  res.json([{id: 1, title: 'Assignment 1 clarification'}, {id: 2, title: 'Exam coverage'}])
-})
+app.get("/coursesPage", (req, res) => {
+  res.json([
+    { id: 1, title: "Assignment 1 clarification" },
+    { id: 2, title: "Exam coverage" },
+  ]);
+});
 
-app.get('/coursesPage/tutorials', (req, res) => {
-  res.json([{id: 1, title: 'Tutorial 1'}, {id: 2, title: 'Tutorial 2'}])
-})
+app.get("/coursesPage/tutorials", (req, res) => {
+  res.json([
+    { id: 1, title: "Tutorial 1" },
+    { id: 2, title: "Tutorial 2" },
+  ]);
+});
 
-app.listen(port, ()=> {
-	console.log(`Backend server listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Backend server listening on port ${port}`);
+});
