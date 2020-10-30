08f3242c9c3a8f33b016a827cbe5a226aaeb4c07
knishibe <knishibeckingham@gmail.com>
2020-10-29 17:56:03 -0700

    added commentID to commentSchema

--- server/models/comments.model.js
+++ server/models/comments.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
+  commentID: {type: BigInt, required: true, unique: true },
   postID: { type: BigInt, required: true },
   userID: { type: string, required: true },
   commentText: { type: string, required: true }
