187e766eafd68bafe90589d02370d14f3b5b93e0
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-10-29 17:34:43 -0700

    Rename route parameters. Start returning JSON responses.
    - Rewrite route parameters to use words like "post" and "comment" rather
      than "thread" and "post".
    - Start writing functions to use mock database and return responses.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 var app = express();
 const port = process.env.PORT || 4000;
-var mockDB = require("./mock_db").mock_DB;
+let mockDB = require("./mock_db").mock_DB;
 
 // THIS IS AN EXAMPLE OF HOW OUR FILES WILL BE SERVED WHEN WE UPLOAD TO GCP
 // app.use(express.static(path.join(__dirname, "..","client", "build")));
@@ -11,24 +11,56 @@ var mockDB = require("./mock_db").mock_DB;
 //   res.sendFile(path.join(__dirname, "..","client", "build", "index.html"))
 // })
 
-// Retrieve a list of all the threads for a class forum.
-app.get("/forum/:subforumId", (req, res) => {});
+// Retrieve a list of all the posts for a certain class.
+app.get("/forum/:classId", (req, res) => {
+  let classId = req.params.classId;
+  res.json(mockDB["posts"]);
+});
 
-// Retrieve all the posts for a particular thread in a particular class forum
-app.get("/forum/:subforumId/:threadId", (req, res) => {});
+// Retrieve all the comments for a post in a specific class
+app.get("/forum/:classId/:postId", (req, res) => {
+  let classId = req.params.classId;
+  let postId = req.params.postId;
+  res.json(mockDB["comments"]);
+});
 // Add a new post for a particular subforum
-app.post("/forum/:subforumId/:threadId", (req, res) => {});
+app.post("/forum/:classId/:postId", (req, res) => {
+  let classId = req.params.classId;
+  let postId = req.params.postId;
+});
 // Delete a particular thread in a class forum
-app.delete("/forum/:subforumId/:threadId", (req, res) => {});
+app.delete("/forum/:classId/:postId", (req, res) => {
+  let classId = req.params.classId;
+  let postId = req.params.postId;
+  // Delete the post with id postId in the  class with classId.
+});
 
 // Update a particular post in a class forum thread
-app.post("/forum/:subforumId/:threadId/:postId", (req, res) => {});
+app.post("/forum/:classId/:postId/:commentId", (req, res) => {
+  let classId = req.params.classId;
+  let postId = req.params.postId;
+  let commentId = req.params.commentId;
+  // Change the text of a comment for a specific post.
+});
 // Delete a particular post in a class forum thread
-app.delete("/forum/:subforumId/:threadId/:postId", (req, res) => {});
+app.delete("/forum/:classId/:postId/:commentId", (req, res) => {
+  let classId = req.params.classId;
+  let postId = req.params.postId;
+  let commentId = req.params.commentId;
+  // Delete a particular comment in the class forum.
+});
 
 // Routing for tutorial list
 // Retrieve a list of tutorials for a particular class
-app.get("/tutorial/:classId/", (req, res) => {});
+app.get("/tutorial/:classId/", (req, res) => {
+  let classId = req.params.classId;
+  let resp = {};
+  for (tutId in mockDB["tutorial"]) {
+    if (mockDB["tutorial"][tutId]["courseID"] == classId) {
+      console.log(mockDB["tutorial"][tutId]);
+    }
+  }
+});
 // Add a new tutorial to the tutorials for a particular class
 app.post("/tutorial/:classId/", (req, res) => {});
 
@@ -42,31 +74,23 @@ app.delete("/tutorial/:classId/:tutorialId", (req, res) => {});
 
 /* Temp routing code */
 // const dashboardRouter = require('./routes/dashboard');
-app.get("/dashboard", (req, res) => {
-  res.json([
-    { id: 1, name: "CMPT 470" },
-    { id: 2, name: "CMPT 471" },
-  ]);
+
+// Retrieve all the classes to be displayed on a certain users dashboard
+app.get("/dashboard/:userId", (req, res) => {
+  let userId = req.params.userId;
+  console.log(userId);
+  let userRecord = mockDB["user"][userId];
+  let classes = {};
+  userRecord["courses"].forEach((className) => {
+    classes[className] = mockDB["courses"][className];
+  });
+  res.json(classes);
 });
 
 app.get("/", (req, res) => {
   res.json({
     message: 'Hello World from the backend server on the "/" route!',
   });
-});
-
-app.get("/coursesPage", (req, res) => {
-  res.json([
-    { id: 1, title: "Assignment 1 clarification" },
-    { id: 2, title: "Exam coverage" },
-  ]);
-});
-
-app.get("/coursesPage/tutorials", (req, res) => {
-  res.json([
-    { id: 1, title: "Tutorial 1" },
-    { id: 2, title: "Tutorial 2" },
-  ]);
 });
 
 app.listen(port, () => {
