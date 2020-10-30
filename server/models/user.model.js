01096041644e056162aca70a1db059306d415cb4
knishibe <knishibeckingham@gmail.com>
2020-10-29 17:31:07 -0700

    added database schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,16 +4,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  userid: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true // trims whitespace off the end
-  },
+  userID: { type: string, required: true, unique: true, trim: true },
   accountType: { type: CharacterData, required: true },
   courses: { type: Array, required: true },
-}, {
-  timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);
