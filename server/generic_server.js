993a7101ef332aac7f3582d845c9df767e43733a
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-11-02 22:28:43 -0800

    Finish implementing generic server for REPL's.
    - Finish implementing the general server which will run REPL's which do
    not buffer stdout or stderr.

--- server/generic_server.js
+++ server/generic_server.js
@@ -2,27 +2,46 @@ const net = require("net");
 const { spawn } = require("child_process");
 
 class GenericServer {
-  constructor(portNum, binName) {
+  constructor(portNum, binName, binFlags) {
     // The port that the server is listening on.
     this.portNum = portNum;
     // The name of the binary which this server is running.
     this.binName = binName;
+    // The flags(if any). Should be an array of one or more strings.
+    this.binFlags = binFlags;
     // The socket used to recieve/send messages.
     this.socket;
-    // The server instance.
-    this.server;
     // Flag indicating if the process is still alive.
     this.procIsLive = false;
     this.shouldKill = false;
-    // Handle to the process running the binary.
-    this.process = spawn(binName);
+    this.currentTimestamp = undefined;
+    this.commandOutput = { type: "SUCCESS", stdout: "", stderr: "" };
 
+    // Start the server on the specified port an listen for connections
+    this.server = new net.createServer();
+    this.server.listen(this.portNum);
+
+    this.server.on("connection", (socket) => {
+      this.socket = socket;
+      this.socket.on("data", (data) => {
+        this.handleIncomingData(data);
+      });
+    });
+
+    // Spawn child process
+    this.process = spawn(binName, binFlags);
+
+    // Add listeners to process events
     this.process.stdout.on("data", (data) => {
       this.handleStdout(data);
     });
     this.process.stderr.on("data", (data) => {
       this.handleStderr(data);
     });
+
+    // At this point, the process should be live.
+    // TODO: Find a better way to do this. At this point, this is just an approximation.
+    this.procIsLive = true;
 
     this.process.on("error", () => {
       this.procIsLive = false;
@@ -32,22 +51,45 @@ class GenericServer {
 
     this.process.on("close", () => {
       this.procIsLive = false;
-      this.killServer;
+      this.killServer();
     });
+  }
 
-    // Check if the process has spawned. If it has not(its simply taking a long time) then
-    // its PID should be undefined.
-    if (typeof this.process.pid != undefined) {
-      this.procIsLive = true;
-      this.server = new net.createServer();
-      this.server.listen(this.portNum);
+  // This method should be overridden by all clients
+  // It should return a string containing a command which echoes a timestamp
+  // This is done to recognize when a "quiet" command(like "cd" in bash) which
+  // makes no output is done running. If this is not done, we have no way of detecting.
+  // It should also set the timestampCommand variable to contain the string version
+  // of the timestamp for comparison.
+  createTimestampCommand() {
+    let date = Date.now().toString();
+    this.currentTimestamp = date;
+    return "echo " + date;
+  }
 
-      this.server.on("connection", (socket) => {
-        this.socket = socket;
-        this.socket.on("data", (data) => {
-          this.handleIncomingData(data);
-        });
-      });
+  handleIncomingData(data) {
+    let req = JSON.parse(data);
+    console.log("GENERAL SERVER: Got data", req);
+
+    // If the connection dies, signal error and kill server
+    if (!this.procIsLive) {
+      this.socket.write(JSON.stringify({ type: "CONNECTION-ERROR" }));
+      this.killServer();
+    }
+
+    if (req["type"] == "RUN") {
+      this.process.stdin.write(req["code"] + "\n");
+      this.timeStamp = this.createTimestampCommand();
+      setTimeout(() => {
+        this.process.stdin.write(this.timeStamp + "\n");
+      }, 1000);
+    } else if (req["type"] == "PING") {
+      this.socket.write(JSON.stringify({ type: "CONNECTION-OK" }));
+    } else if (req["type"] == "RUNANDKILL") {
+      this.process.stdin.write(req["code"] + "\n");
+      this.shouldKill = true;
+    } else if (req["type"] == "KILL") {
+      this.killServer();
     }
   }
 
@@ -60,35 +102,48 @@ class GenericServer {
   }
 
   handleStdout(data) {
-    let respObject = { stdout: data, stderr: "" };
-    this.socket.write(JSON.stringify(respObject));
+    //let respObject = { type: "SUCCESS", stdout: data.toString(), stderr: "" };
+    // If we get the current timestamp we are waiting for, the command has executed
+    // and produced an output.
+    if (data.toString().trim() === this.currentTimestamp) {
+      console.log("GENERAL SERVER: Sending STDOUT data", this.commandOutput);
+      this.socket.write(JSON.stringify(this.commandOutput));
+      this.commandOutput = { type: "SUCCESS", stdout: "", stderr: "" };
+    } else {
+      console.log(
+        "GENERAL SERVER: Appending stdout data to command output.",
+        data.toString()
+      );
+      // Otherwise, we append the output.
+      this.commandOutput["stdout"] =
+        this.commandOutput["stdout"] + data.toString();
+    }
+
+    // If we have to, kill the server
     if (this.shouldKill) {
       this.killServer();
     }
   }
+
   handleStderr(data) {
-    let respObject = { stdout: "", stderr: data };
-    this.socket.send(JSON.stringify(respObject));
+    if (data.toString().trim() === this.currentTimestamp) {
+      console.log("GENERAL SERVER: Sending STDOUT data", this.commandOutput);
+      this.socket.write(JSON.stringify(this.commandOutput));
+      this.commandOutput = { type: "SUCCESS", stdout: "", stderr: "" };
+    } else {
+      console.log(
+        "GENERAL SERVER: Appending stdout data to command output.",
+        data.toString()
+      );
+      // Otherwise, we append the output.
+      this.commandOutput["stderr"] =
+        this.commandOutput["stderr"] + data.toString();
+    }
+
     if (this.shouldKill) {
       this.killServer();
-    }
-  }
-
-  handleIncomingData(data) {
-    let req = JSON.parse(data);
-
-    if (!this.procIsLive) {
-      this.socket.write(JSON.stringify({ type: "CONNECTION-ERROR" }));
-      this.killServer();
-    }
-
-    if (req["type"] == "RUN") {
-      this.process.stdin.write(req["code"]);
-    } else if (req["type"] == "PING") {
-      this.socket.write(JSON.stringify({ type: "CONNECTION-OK" }));
-    } else if (req["type"] == "RUNANDKILL") {
-      this.process.stdin.write(req["code"]);
-      this.shouldKill = true;
     }
   }
 }
+
+module.exports.GenericServer = GenericServer;
