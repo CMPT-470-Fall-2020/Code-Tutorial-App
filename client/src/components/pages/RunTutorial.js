a62b3ba2ce3f38f726a01d6c8478bd6d4d3896b5
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-11-08 01:25:31 -0800

    Modify tutorial display to use custom components.
    - Modify tutorial display to use a custom component for markdown cells.

--- client/src/components/pages/RunTutorial.js
+++ client/src/components/pages/RunTutorial.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react'
 import {Container} from 'react-bootstrap';
 import Header from './../layout/Header';
-import axios from 'axios';
 import {marked} from './markdownParser';
+import JsxParser from 'react-jsx-parser'
+import axios from 'axios';
+
+
+class MarkdownCell extends Component{
+	constructor(props){
+		super(props)
+		console.log("Cell init with", this.props.code);
+		this.state = {
+			code: this.props.code,
+			lang: this.props.lang,
+			interpName: this.props.iname,
+			codeOutput: "",
+		}
+        this.buttonStyle = 'style={{font-size: 12px; float: right; border: solid 1px black; margin-top: 1%}}';
+        console.log("This state", this.state);
+	  this.runCode= this.runCode.bind(this);
+	}
+	
+	runCode(){
+        axios({
+            method: "POST",
+            data: {
+					uname: "someuser", //TODO: We need to store the username or user id of a person logged in.
+					iname: this.interpName,
+					code: this.code,
+					lang: this.lang
+            },
+            url: `/run`,
+          }).then( res => {
+          	 let retMsg = res.data.message;
+          	 console.log(retMsg, "and this", this);
+
+          	 if(retMsg.type == "SUCCESS"){
+          	 	if (retMsg.stdout.length != 0){
+					this.setState({codeOutput:retMsg.stdout})
+          	 	}else{
+					this.setState({codeOutput:retMsg.stderr})
+          	 	}
+          	 }
+        })
+	}
+
+	render() {
+			console.log("from top of render", this.runCode);
+			const divStyle = {width:"100%", height:"auto", border: "solid 1px #DFDFDF", background: "#F7F7F7", padding:"0.5%"};
+			return(
+            <code>
+            	<pre>
+            		<div style={divStyle}>{this.state.code}</div>
+            		<button onClick={this.runCode}>Run code cell</button>
+            		<div>{this.state.codeOutput}</div>
+            	</pre>
+            </code>
+			)
+	}
+}
+
+class ExtendedComponent extends JsxParser{
+	constructor(props){
+		super(props);
+		console.log("Extended class state", this)
+	}
+}
 
 export default class RunTutorial extends Component {
 
@@ -17,6 +80,7 @@ export default class RunTutorial extends Component {
     }
 
     componentDidMount() {
+    	console.log("from tutorail", axios);
         // Get tutorial from server
         axios.get(`http://localhost:4000/tutorial/${this.state.courseID}/${this.state.tutorialID}`)
           .then((res) => {
@@ -32,14 +96,27 @@ export default class RunTutorial extends Component {
         });
     }
 
+    clickBtn(){
+    	console.log("activated")
+        console.log(this.state.renderedHTML)
+    }
+
+    //<div dangerouslySetInnerHTML={{__html:this.state.renderedHTML}}></div>
     render() {
         return (
+        	<div>
             <React.Fragment>
                 {this.props.location.pathname !== '/login' && <Header />}
                 <Container style={tutorialStyle}>
-                    <div dangerouslySetInnerHTML={{__html:this.state.renderedHTML}}></div>
+                    Random Text
+				   <JsxParser
+				    components={{MarkdownCell}}
+					jsx={this.state.renderedHTML}
+				    blacklistedAttrs={[]}
+					/>
                 </Container>
             </React.Fragment>
+            </div>
         )
     }
 }
