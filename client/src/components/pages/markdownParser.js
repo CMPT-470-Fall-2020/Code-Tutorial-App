ba71d33fc6e932c42580e9c08ed4162cc908009e
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-11-11 18:51:47 -0800

    Refactor parser to handle theme/keybind choices. Delete dead code.

--- client/src/components/pages/markdownParser.js
+++ client/src/components/pages/markdownParser.js
@@ -8,20 +8,8 @@ markdownRenderer.setOptions({
 
 const renderer = {
     code(code, info, escaped) {
-        //var divStyle = 'style={{width:100%; height:auto; border: solid 1px #DFDFDF; background: #F7F7F7; padding:0.5%}}';
-        //var buttonStyle = 'style={{font-size: 12px; float: right; border: solid 1px black; margin-top: 1%}}';
-        /*
-        return (
-            `<code>
-            	<pre>
-            		<div >${code}</div>
-            		<button onClick={sendMe}>{foo}</button>
-            	</pre>
-            	</code>`
-        )
-        */
         let cellAttr = info.split(" ");
-        return `<MarkdownCell userid={\"{userId}\"} shouldRun={shouldRunCells} lang={\"${cellAttr[0]}\"} iname={\"${cellAttr[1]}\"} code={\`${code}\`}/>`
+        return `<MarkdownCell userid={\"{userId}\"} shouldRun={shouldRunCells} theme={theme} keymap={keymap} lang={\"${cellAttr[0]}\"} iname={\"${cellAttr[1]}\"} code={\`${code}\`}/>`
     }
 };
 
