88787d18faf8d276a18e45e2eec4cbb2992c3c9a
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-11-23 16:29:54 -0800

    Convert logger to a singleton.

--- server/logging.js
+++ server/logging.js
@@ -1,7 +1,39 @@
-const log4js = require("log4js");
+const log4js = require('log4js');
+
+// Source: https://gist.github.com/ericwastaken/11d0230f4f7dd57ef4907cd6250d985f
+
+let SharedLog = (function() {
+  // Instance stores a reference to the Singleton
+  let _sharedLog;
+  let _logger;
+
+  // Initialize logger and return a single instance
+  function init() {
+    // We don't have a shared log instance yet, so let's create it and store it
+    _sharedLog = this;
+
+    // Configure the logger
 log4js.configure({
   appenders: { 'code-exec': { type: "file", filename: "./server-logs/code-exec.log", maxLogSize: 2048 } },
   categories: { default: { appenders: ["code-exec"], level: "debug" } }
 });
- 
-module.exports = log4js.getLogger("code-exec");
+    // Create an instance as configured
+    _logger = log4js.getLogger("code-exec");
+
+    return {
+      logger: _logger,
+    };
+  }
+
+  return {
+  	// Returns an instance or initialize a new one if it does not exist
+    getInstance: function() {
+      if (!_sharedLog) {
+        _sharedLog = init();
+      }
+      return _sharedLog;
+    },
+  };
+})();
+
+module.exports = SharedLog;
