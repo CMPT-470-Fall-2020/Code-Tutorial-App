d24e414a7b3a531acdbff25edf1bfb21c2a9c957
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-11-29 12:59:12 -0800

    Add a powershell file.

---
+++ server/Containers/generate-containers.ps1
@@ -0,0 +1,49 @@
+# Remove all existing images if they exist.
+# If they don't exist, this command will spit out an error but its ok.
+docker image rm 470-ubuntu-base
+docker image rm 470-ubuntu-bash
+docker image rm 470-ubuntu-zsh
+docker image rm 470-ubuntu-julia
+docker image rm 470-ubuntu-python
+
+# Build the base container used to support all other language containers
+Write-Host "Building base ubuntu container"
+cd ./basic-ubuntu
+docker build -t 470-ubuntu-base .
+
+# Build the base container for bash
+Write-Host "Building base bash container"
+cd ../ubuntu-bash
+# Some linux distros alias cp to "cp -i". This causes issues with the script
+# since it will ask for permission before overwritting any existing files overwrite.
+copy ../../language-servers/bash-server.js ./
+copy ../../generic_server.js ./
+docker build -t 470-ubuntu-bash .
+
+
+# Build the base container for bash
+Write-Host "Building base zsh container"
+cd ../ubuntu-zsh
+# Some linux distros alias cp to "cp -i". This causes issues with the script
+# since it will ask for permission before overwritting any existing files overwrite.
+copy ../../language-servers/zsh-server.js ./
+copy ../../generic_server.js ./
+docker build -t 470-ubuntu-zsh .
+
+# Build the base container for bash
+Write-Host "Building base julia container"
+cd ../ubuntu-julia
+# Some linux distros alias cp to "cp -i". This causes issues with the script
+# since it will ask for permission before overwritting any existing files overwrite.
+copy ../../language-servers/julia-server.js ./
+copy ../../generic_server.js ./
+docker build -t 470-ubuntu-julia .
+
+
+# Build the base container for bash
+Write-Host "Building base python container"
+cd ../ubuntu-python
+# Some linux distros alias cp to "cp -i". This causes issues with the script
+# since it will ask for permission before overwritting any existing files overwrite.
+copy ../../language-servers/python-server.py ./
+docker build -t 470-ubuntu-python .
