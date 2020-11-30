5ed8b7819bff100e76246c844cc05b9d1215e7eb
knishibe <knishibeckingham@gmail.com>
2020-11-30 10:49:07 -0800

    added file support to db (not sure if it works correctly but need other functionality to test)

--- server/models/autograder.model.js
+++ server/models/autograder.model.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const autograderSchema = new Schema({
   userID: { type: mongoose.Types.ObjectId, required: true },
   courseID: { type: mongoose.Types.ObjectId, required: true },
-  code: { type: String, required: true },
+  fileName: { type: String, required: true },
   testName: { type: String, required: true },
 }, {
   timestamps: true
--- server/routes/autograder.js
+++ server/routes/autograder.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
 const Autograder = require('./../models/autograder.model');
 
 // Add a new Test 
 router.route("/:classId/add").post((req, res) => {
     let courseID = req.params.classId;
     let userID = req.body.userID;
-    let code = req.body.code;
     let testName = req.body.testName;
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path; 
+      var newpath = '../../../files/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+      });
+    });
+
+    let fileName =  Date.now() + userID + "-" + courseID + "-test." + path.extname(oldpath);
   
-    let newTest = new Autograder({userID, courseID, code, testName});
-    newCourse.save()
+    let newTest = new Autograder({userID, courseID, fileName, testName});
+    newTest.save()
       .then(() => res.json('Test added!'))
       .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -18,9 +31,14 @@ router.route("/:classId/add").post((req, res) => {
 router.route("/").delete((req, res) => {
     let testID = req.body.testID;
   
-    Autograder.findByIdAndDelete(testID)
-      .then(() => res.json('Test deleted.'))
+    Autograder.findById(testID)
+      .then(test)
       .catch(err => res.status(400).json('Error: ' + err));
+
+    fs.unlink('../../../files/' + test.fileName, function (err) {
+      if (err) throw err;
+      res.json('Test deleted!');
+    });
   
 });
 
