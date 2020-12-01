ca1652e205655ba045758b4730292f87130b5e47
Emily <eca83@sfu.ca>
2020-11-30 21:16:47 -0800

    Update delete test route

--- server/routes/autograder.js
+++ server/routes/autograder.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-var formidable = require('formidable');
 var multer = require('multer');
 var fs = require('fs');
 var path = require('path');
@@ -29,7 +28,7 @@ router.route("/:classId/add").post((req, res) => {
       }
       // rename file name to have user id
       fs.rename(req.file.path, req.file.path.replace('userID', userID).replace('courseID', courseID), () => {
-        var fileName = userID + '-' + courseID + '-' + req.file.originalname
+        var fileName = req.file.path.replace('userID', userID).replace('courseID', courseID)
         var testName = req.file.originalname
 
         let newTest = new Autograder({userID, courseID, fileName, testName});
@@ -41,18 +40,17 @@ router.route("/:classId/add").post((req, res) => {
 });
   
 // Delete a Test 
-router.route("/:classId/:testId").delete((req, res) => {
-    let testID = req.params.testID;
+router.route("/:classId/:testId/tests/:fileName").delete((req, res) => {
+    let testID = req.params.testId;
+    let fileName = req.params.fileName;
 
-    Autograder.findById(testID)
-      .then(test)
-      .catch(err => res.status(400).json('Error: ' + err));
-
-    fs.unlink('../../../files/' + test.fileName, function (err) {
+    Autograder.findByIdAndDelete(testID)
+    .then(() => res.json('Test deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+    let filepath = path.join(__dirname, '../tests/', fileName);
+    fs.unlink(filepath, function (err) {
       if (err) throw err;
-      res.json('Test deleted!');
     });
-  
 });
 
 // Get a Test 
@@ -67,7 +65,8 @@ router.route("/:classId/:testId").get((req, res) => {
 
 // Get all Tests for a class
 router.route("/:classId").get((req, res) => {
-  
+  let classId = req.params.classId;
+
     Autograder.find({'courseID': classId})
       .then(tests => res.json(tests))
       .catch(err => res.status(400).json('Error: ' + err));
