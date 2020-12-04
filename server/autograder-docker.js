6e3119daa3d1985d9b7060faccf6aac344afb2f5
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-12-04 03:20:57 -0800

    Refactor code. Implement remainder of autograder.

--- server/autograder-docker.js
+++ server/autograder-docker.js
@@ -10,6 +10,20 @@ const chance = new Chance();
 const DOCKER_NAME_POOL =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+const BASE_IMAGES = {
+  bash: "470-ubuntu-bash-test",
+  zsh: "470-ubuntu-zsh-test",
+  python: "470-ubuntu-python-test",
+  julia: "470-ubuntu-julia-test",
+};
+
+const COMMANDS = {
+  bash: "470-ubuntu-bash-test",
+  zsh: "470-ubuntu-zsh-test",
+  python: ["python3", "professorTest.py"],
+  julia: "470-ubuntu-julia-test",
+};
+
 class AutograderDockerInstance {
   constructor(imageLang) {
     logger.info(
@@ -19,8 +33,15 @@ class AutograderDockerInstance {
     //this.imgType = imageLang;
     //this.baseImg = BASE_IMAGES[imageLang.toLowerCase()];
     this.container_name = chance.string({ pool: DOCKER_NAME_POOL });
-    this.docker = new Docker();
+    this.docker = new Docker(); // create new docker instance
+    // This is set after the container is started.
     this.container_instance = undefined;
+    this.pollId = undefined;
+    // Store the output of the exec command's stdout and stderr.
+    this.execStdout = "";
+    this.execStderr = "";
+    // Holds the callback function used send data back to client.
+    this.respCallback = undefined;
 
     logger.trace(
       "DOCKER: Constructor finished. Container name will be",
@@ -29,44 +50,53 @@ class AutograderDockerInstance {
   }
 
   execInContainer(callback) {
-    this.container_instance.exec(
-      {
+    this.container_instance
+      .exec({
         Cmd: ["python3", "professorTest.py"],
         attachStdout: true,
         attachStderr: true,
         WorkingDir: "/home",
-      },
-      (err, exec) => {
-        console.log("created exec command");
-        if (err) {
-          console.log("exec got an error");
-          throw err;
-        } else {
-          console.log("Exec command is created.");
-          exec.start({}, (err, stream) => {
-            console.log("exec is starting");
-            //console.log("stdout",process.stdout)
-            let outputStdout = "";
-            let logStream = new PassThrough();
-            logStream.on("data", function (chunk) {
-              outputStdout += chunk.toString("utf8");
-              console.log("data 1", outputStdout);
-              callback(outputStdout);
-            });
+      })
+      .then((execObject) => {
+        execObject.start({ hijack: true, stdin: false }, (err, stream) => {
+          console.log("EXEC: Command is starting.");
+          // Store the refernce to this class in a new variable so it does not get overwritten
+          // by the events used to collect output.
+          let self = this;
+          let stdout = new PassThrough();
+          let stderr = new PassThrough();
 
-            logStream.on("end", function () {
-              console.log("output from exec command end", outputStdout);
-            });
-            logStream.on("finish", function () {
-              console.log("output from exec command finish", outputStdout);
-            });
-
-            this.docker.modem.demuxStream(stream, logStream, process.stderr);
+          // Set events for errors
+          stderr.on("data", function (chunk) {
+            self.execStderr += chunk.toString("utf8");
           });
-        }
-      }
-    );
+
+          stdout.on("data", function (chunk) {
+            self.execStdout += chunk.toString("utf8");
+          });
+
+          this.docker.modem.demuxStream(stream, stdout, stderr);
+
+          this.pollId = setInterval(() => {
+            execObject.inspect().then((resp) => {
+              // If the process we are trying to execute has stopped running, we can send back
+              // the response to the user.
+              if (resp.Running === false) {
+                console.log("EXEC: Instance Died x(");
+                // If the object is dead, we want to send back the data and kill the container.
+                self.respCallback(self.execStdout);
+                clearInterval(self.pollId);
+                self.stopInstance();
+              }
+            });
+          }, 3000);
+        });
+      })
+      .catch((err) => {
+        console.log("error when creating exec object", err);
+      });
   }
+
   // Src: https://github.com/apocas/dockerode/issues/240
   // Even though the docker API can use "gzip", for some reason the
   // container cannot unzip it.
@@ -76,36 +106,45 @@ class AutograderDockerInstance {
     studentCode,
     responseCallback
   ) {
+    // Store the callback used to send code back to the user.
+    this.respCallback = responseCallback;
+
     let professorFile = fs.readFile(localFileName, "utf-8", (err, data) => {
       // Bind this to self variable so there is no name clash inside of callbacks.
       let self = this;
       console.log("err file", err);
-      console.log("SENDARCHIVE:", data, "AND STUDENT CODE:", studentCode);
+      console.log(
+        "SENDARCHIVE TEACHER CODE:\n",
+        data,
+        "AND STUDENT CODE:\n",
+        studentCode,
+        "end\n"
+      );
 
+      // Create a new tarfile
       let pack = tar.pack();
       pack.entry({ name: instanceFileName }, data);
+      // TODO: Set file ending based on the type of file(.py, .sh, .zsh....)
       pack.entry({ name: "studentCode.py" }, studentCode);
       pack.finalize();
 
-      var chunks = [];
+      var tarFileChunks = [];
+      // The tarfile is a stream of data.
+      // Each time a new chunk of data is created, we append it to the pack
       pack.on("data", function (chunk) {
-        chunks.push(chunk);
+        tarFileChunks.push(chunk);
       });
       pack.on("end", function () {
-        var buffer = Buffer.concat(chunks);
-        self.container_instance.putArchive(
-          buffer,
-          { path: "/home" },
-          function (error, response) {
-            if (error) {
-              console.log("Error transporting file", error);
-            } else {
-              console.log("Finished transport. Exec command now.");
-
-              self.execInContainer(responseCallback);
-            }
-          }
-        );
+        var buffer = Buffer.concat(tarFileChunks);
+        self.container_instance
+          .putArchive(buffer, { path: "/home" })
+          .then(() => {
+            console.log("Success transporting file");
+            self.execInContainer(responseCallback);
+          })
+          .catch((err) => {
+            console.log("Error transporting file");
+          });
       });
     });
   }
@@ -149,24 +188,11 @@ class AutograderDockerInstance {
     this.container_instance.stop((err, data) => {
       if (err) {
         logger.error("Instance stopped with an error:", err);
-        return false;
       }
       logger.info("Instance stopped succesfully");
-      return true;
     });
   }
-
-  // TODO implement check for container being alive
-  isAlive() {}
 }
-
-/*
-let a = new AutograderDockerInstance("a", 44);
-a.startInstance(() => {
-  console.log("Instance Started!");
-  a.sendArchiveToInstance("./profTest.py", "professorTest.py");
-});
-*/
 
 module.exports = {
   AutoGraderInstance: AutograderDockerInstance,
