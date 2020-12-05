01f3a67b9994073725c2253428c1466e9190c387
Yavor Konstantinov <ykonstantinov1@gmail.com>
2020-12-04 21:37:51 -0800

    Add tests. fix running python/zsh/bash tests.

--- server/tests/1607141726925-5fa21bbfc1c1c56142697eef-5fa0b53e3ec4a03f3c6eb512-this is a new test.py
+++
@@ -1,5 +0,0 @@
-import studentCode
-import math
-print("this is an example test")
-print("Hello from the test!")
-print(math.log2(4))
---
+++ server/tests/1607146171980-5fa21bbfc1c1c56142697eef-5fa0b53e3ec4a03f3c6eb561-fibTest.py
@@ -0,0 +1,19 @@
+from studentCode import fib
+
+# n = 3
+if (fib(3) == 2):
+    print("n = 3 passed the fib test")
+else:
+    print("n = 3 failed the fib test")
+
+# n = 0
+if (fib(0) == 0):
+    print("n = 0 passed the fib test")
+else:
+    print("n = 0 failed the fib test")
+
+# n = 23
+if (fib(23) == 28657):
+    print("n = 23 passed the fib test")
+else:
+    print("n = 23 failed the fib test")
